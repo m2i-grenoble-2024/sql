4857d34f8e1b87c92b2321fce27f0d6519963226
Jean Demel <demeljean@hotmail.fr>
2024-06-11 09:50:58 +0200

    added insert into

--- database.sql
+++ database.sql
@@ -12,16 +12,15 @@ CREATE TABLE address(
     city VARCHAR(255),
     zip_code VARCHAR(20),
     person_id INT NOT NULL REFERENCES person(id)
+    -- FOREIGN KEY person_id REFERENCES person(id) -- On peut aussi déclarer la FK à part
 );
 
-INSERT INTO address (person_id) VALUES (1);
-DELETE FROM person WHERE id=1;
+-- ALTER TABLE address ADD FOREIGN KEY (person_id) REFERENCES person(id);
 
--- Créer la table address avec ses 4 propriétés propres toutes en varchar sauf l'id
--- mais également une colonne person_id en INT qui ne pourra pas être null
--- Chercher sur internet comment faire en sorte que cette colonne person_id soit une
--- FOREIGN KEY qui fait référence à l'id de la table person
+INSERT INTO person (name,first_name,age) VALUES
+('Sakhri', 'Lisa', 65),
+('Sakhri', 'Abdala', 30),
+('Gomex', 'Lisa', 25),
+('Johnson', 'Alberto', 65),
+('Mouriel', 'Baptiste', 18);
 
--- Pour voir si ça marche, essayer de faire d'abord un INSERT INTO address en lui
--- donnant une person_id existante (genre 1) puis essayer avec une person_id qui n'existe
--- pas (genre 2000), si la foreign key est bien faite, ça devrait causer une erreur
