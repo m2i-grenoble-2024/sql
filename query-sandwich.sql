a330c9c68177580754036fc4676bb7072557f5a4
Jean Demel <demeljean@hotmail.fr>
2024-06-13 17:01:46 +0200

    queries

---
+++ query-sandwich.sql
@@ -0,0 +1,30 @@
+-- Commencer une nouvelle commande
+INSERT INTO commande (order_date,status) VALUES (NOW(), 'awaiting');
+
+-- Récupérer les sandwich d'une commande donnée
+SELECT * FROM sandwich 
+LEFT JOIN commande ON commande.id=sandwich.commande_id 
+WHERE commande_id=6;
+
+-- Ajouter un sandwich à une commande
+INSERT INTO sandwich (commande_id) VALUES (6);
+
+-- Afficher la liste des pains puis des prot, puis des garnish, puis des sauces
+SELECT * FROM ingredient WHERE type='bread';
+SELECT * FROM ingredient WHERE type='protein';
+SELECT * FROM ingredient WHERE type='garnish';
+SELECT * FROM ingredient WHERE type='sauce';
+-- Ajouter un ingrédient à un sandwich
+INSERT INTO sandwich_ingredient (ingredient_id,sandwich_id) VALUES (2, 7);
+
+-- Afficher le récap d'un sandwich
+SELECT * FROM sandwich
+INNER JOIN sandwich_ingredient ON sandwich.id=sandwich_ingredient.sandwich_id
+INNER JOIN ingredient ON sandwich_ingredient.ingredient_id=ingredient.id
+WHERE sandwich.id=5;
+
+SELECT *, SUM(ingredient.price) AS total FROM sandwich
+INNER JOIN sandwich_ingredient ON sandwich.id=sandwich_ingredient.sandwich_id
+INNER JOIN ingredient ON sandwich_ingredient.ingredient_id=ingredient.id
+WHERE sandwich.id=5
+GROUP BY sandwich.id;
