0d533960c1d7d18be8d24bfa87dfdc1b802f7a56
Jean Demel <demeljean@hotmail.fr>
2024-06-17 09:53:44 +0200

    instruction query

--- bibliothèque/query.sql
+++ bibliothèque/query.sql
@@ -1,1 +1,4 @@
 -- Faire une requête pour récupérer les emprunts EN COURS d'un⋅ adhérent⋅e avec le livre concerné ainsi que la date de retour prévue
+
+-- Faire une requête pour rechercher un auteur à la fois par son titre,description,genre, nom et prénom d'auteur
+
