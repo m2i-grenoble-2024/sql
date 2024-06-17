5c8ec90608adc7e2571bb93428f5b3c70626c431
Jean Demel <demeljean@hotmail.fr>
2024-06-17 11:09:44 +0200

    query + instructions

--- bibliothèque/query.sql
+++ bibliothèque/query.sql
@@ -1,4 +1,14 @@
 -- Faire une requête pour récupérer les emprunts EN COURS d'un⋅ adhérent⋅e avec le livre concerné ainsi que la date de retour prévue
+SELECT livre.titre, emprunt.date_emprunt+INTERVAL jours_autorises DAY FROM emprunt 
+INNER JOIN livre ON livre.id=emprunt.id_livre
+WHERE emprunt.id_utilisateur=1 AND emprunt.date_retour_reelle IS NULL;
 
--- Faire une requête pour rechercher un auteur à la fois par son titre,description,genre, nom et prénom d'auteur
+-- Faire une requête pour rechercher un livre à la fois par son titre,description,genre, nom et prénom d'auteur
+SELECT * FROM livre
+LEFT JOIN auteur_livre ON livre.id=auteur_livre.id_livre
+LEFT JOIN auteur ON auteur_livre.id_auteur=auteur.id
+WHERE CONCAT(livre.titre,livre.description,livre.genre,auteur.nom,auteur.prenom) LIKE '%es%';
 
+-- Faire une requête pour récupérer la liste des adhérents
+
+-- Faire une requête pour récupérer la liste des amendes non payées et le nom/prénom de l'utilisateur⋅ice à qui est assignée l'amende
